d73f313a9cbe9ef9d696f7b249da46d733c2ed20
comteharbour <florian.pittet@softozor.ch>
2019-04-18 17:30:01 +0200

    fix i18n bugs

--- src/components/I18n/LanguageSelect.vue
+++ src/components/I18n/LanguageSelect.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="locale-changer">
-    <q-select v-model="$i18n.locale" :options="langs" dark />
+    <q-select v-model="$i18n.locale" :options="['fr', 'en'] " dark />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LanguageSelect',
-  data () {
-    return {
-      langs: [
-        {
-          label: 'fr',
-          value: 'fr'
-        },
-        {
-          label: 'en',
-          value: 'en'
-        }
-      ]
-    }
-  }
+  name: 'LanguageSelect'
 }
 </script>
