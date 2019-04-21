75046d900229c53e72ff81081a0b552055624899
comteharbour <florian.pittet@softozor.ch>
2019-04-22 01:55:45 +0200

    working basic map

--- src/components/map/markers/BudzonneryMarker.vue
+++ src/components/map/markers/BudzonneryMarker.vue
@@ -1,11 +1,16 @@
 <template>
-  <l-marker :lat-lng="latLng" :icon="icon" />
+  <l-marker :lat-lng="latLng" :icon="icon">
+    <l-popup>
+      <budzonnery-card :name="name" :image="image" />
+    </l-popup>
+  </l-marker>
 </template>
 
 <script>
 import Budzi from './budzi_shadow.png'
 import Crown from './crown.png'
 import L from 'leaflet'
+import BudzonneryCard from './BudzonneryCard'
 
 export default {
   name: 'BudzonneryIcon',
@@ -17,6 +22,14 @@ export default {
     marked: {
       type: Boolean,
       default: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      default: undefined
     }
   },
   computed: {
@@ -24,7 +37,7 @@ export default {
       const iconUrl = Budzi
       const iconSize = [72, 63]
       const iconAnchor = [22, 59]
-      const popupAnchor = [-3, -76]
+      const popupAnchor = [0, -72]
       const shadowUrl = Crown
       const shadowSize = [51, 28]
       const shadowAnchor = [25, 76]
@@ -34,6 +47,7 @@ export default {
       if (this.marked) return L.icon(markedProps)
       else return L.icon(blankProps)
     }
-  }
+  },
+  components: {BudzonneryCard}
 }
 </script>
