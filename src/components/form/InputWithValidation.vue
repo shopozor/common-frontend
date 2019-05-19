09c7fcb6dfb35ebd4c85bbd8dad54b2376c35a10
comteharbour <florian.pittet@softozor.ch>
2019-05-19 15:52:36 +0200

    component: input with validation

---
+++ src/components/form/InputWithValidation.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-input
+    v-model="value"
+    :type="type"
+    :float-label="floatLabel"
+    :label="label"
+    :hint="hint"
+    :error-message="errorMessage"
+    @input="$emit('input', $event)"
+    :error="showError && knowError"
+    no-error-icon>
+    <template v-slot:before>
+      <q-icon :name="iconName" />
+    </template>
+    <template v-slot:append>
+      <validity-icon
+        :knowError="knowError"
+        :showError="showError"
+        :mandatory="mandatory" />
+    </template>
+  </q-input>
+</template>
+
+<script>
+import ValidityIcon from './ValidityIcon'
+
+export default {
+  name: 'InputWithValidation',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    floatLabel: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    hint: {
+      type: String,
+      default: ''
+    },
+    errorMessage: {
+      type: String,
+      default: ''
+    },
+    iconName: {
+      type: String,
+      default: 'edit'
+    },
+    showError: {
+      type: Boolean,
+      default: false
+    },
+    knowError: {
+      type: Boolean,
+      default: true
+    },
+    mandatory: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: { ValidityIcon }
+}
+</script>
