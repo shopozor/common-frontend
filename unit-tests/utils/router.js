efc3b4733b05a6b3823f19609d4f9bb2f158050e
comteharbour <florian.pittet@softozor.ch>
2019-06-15 14:19:31 +0200

    include $router mock

--- unit-tests/utils/router.js
+++ unit-tests/utils/router.js
@@ -1,7 +1,7 @@
 import VueRouter from 'vue-router'
 
 export function initRouter (localVue, options) {
-  if (options.mocks && options.mocks.$route) {
+  if (options.mocks && (options.mocks.$route || options.mocks.$router)) {
     return {}
   } else {
     localVue.use(VueRouter)
