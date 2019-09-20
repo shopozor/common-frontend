b46965fbd6fd5cc1e6406e96bd3a487ee7a1d9c3
Laurent Michel <laurent.michel@softozor.ch>
2019-09-20 12:43:18 +0200

    add everything necessary to run the repo's unit tests (and probably more than required)

---
+++ jest.config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  globals: {
+    __DEV__: true
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/test/jest/jest.setup.js'
+  ],
+  // noStackTrace: true,
+  // bail: true,
+  // cache: false,
+  // verbose: true,
+  // watch: true,
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/test/jest/coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.vue',
+    '<rootDir>/src/components/**/*.js',
+    '<rootDir>/src/pages/**/*.vue',
+    '<rootDir>/src/pages/**/*.js',
+    '<rootDir>/src/store/**/*.vue',
+    '<rootDir>/src/store/**/*.js',
+    '<rootDir>/common/src/*.vue',
+    '<rootDir>/common/src/*.js',
+    '<rootDir>/common/src/components/**/*.vue',
+    '<rootDir>/common/src/components/**/*.js',
+    '<rootDir>/common/src/mixins/**/*.vue',
+    '<rootDir>/common/src/mixins/**/*.js',
+    '<rootDir>/common/src/router/**/*.vue',
+    '<rootDir>/common/src/router/**/*.js',
+    '<rootDir>/common/src/store/**/*.vue',
+    '<rootDir>/common/src/store/**/*.js'
+  ],
+  coverageThreshold: {
+    global: {
+    //  branches: 50,
+    //  functions: 50,
+    //  lines: 50,
+    //  statements: 50
+    }
+  },
+  testMatch: [
+    // '<rootDir>/test/jest/__tests__/**/*.spec.js',
+    // '<rootDir>/test/jest/__tests__/**/*.test.js',
+    '<rootDir>/src/**/__tests__/*.spec.js',
+    '<rootDir>/common/src/**/__tests__/*.spec.js'
+  ],
+  moduleFileExtensions: [
+    'vue',
+    'js',
+    'jsx',
+    'json',
+    'ts',
+    'tsx'
+  ],
+  moduleNameMapper: {
+    '^vue$': '<rootDir>/node_modules/vue/dist/vue.common.js',
+    '^test-utils$': '<rootDir>/node_modules/@vue/test-utils/dist/vue-test-utils.js',
+    '^quasar$': '<rootDir>/node_modules/quasar/dist/quasar.common.js',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '.*css$': '<rootDir>/test/jest/utils/stub.css'
+  },
+  transform: {
+    '.*\\.vue$': 'vue-jest',
+    '.*\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
+    // use these if NPM is being flaky
+    // '.*\\.vue$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/vue-jest',
+    // '.*\\.js$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!quasar/lang)'
+  ],
+  snapshotSerializers: [
+    '<rootDir>/node_modules/jest-serializer-vue'
+  ]
+}
