8471c83f52ff9f143cf9595d0598d19b7e9586b8
Laurent Michel <laurent.michel@ruag.com>
2019-09-27 08:29:11 +0200

    add debug info

--- PublishToDocker.groovy
+++ PublishToDocker.groovy
@@ -9,6 +9,11 @@ pipeline {
     stage('Build and publish docker image') {
       steps {
         script {
+          echo "REPO = $REPO"
+          echo "BRANCH = $BRANCH"
+          echo "GRAPHQL_API = $GRAPHQL_API"
+          echo "ENABLE_DEV_TOOLS = $ENABLE_DEV_TOOLS"
+          echo "IMAGE_TYPE = $IMAGE_TYPE"
           helpers.publishDockerImage(REPO, BRANCH, GRAPHQL_API, ENABLE_DEV_TOOLS, IMAGE_TYPE)
         }
       }
