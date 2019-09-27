e8e317ea2ee71cbcf615696cee9bd15554640fe4
Laurent Michel <laurent.michel@ruag.com>
2019-09-27 07:32:51 +0200

    refactor deployment script

--- e2e/deploy-to-jelastic.sh
+++ e2e/deploy-to-jelastic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 5 ] ; then
-  echo "Usage: $0 hosterUrl appId login password envName [deploy_group = cp] [path-to-manifest = manifest.jps]"
+  echo "Usage: $0 hosterUrl appId login password envName [deploy_group = cp] [path-to-manifest = manifest.jps] [tag = latest]"
   exit 0
 fi
 
@@ -14,7 +14,7 @@ SESSION=$(getSession $3 $4 ${HOSTER_URL})
 ENV_NAME=$5
 DEPLOY_GROUP=${6:-cp}
 MANIFEST=${7:-manifest.jps}
-CONTEXT="ROOT"
+TAG=${8:-latest}
 
 wasEnvCreated() {
   echo "envName = $2" >&2
@@ -30,68 +30,28 @@ redeployEnvironment() {
   local session=$1
   local envName=$2
   local deployGroup=$3
+  local tag=$4
   echo "Redeploying group <$deployGroup> of environment <$envName>" >&2
   local cmd=$(curl -k \
     -H "${CONTENT_TYPE}" \
     -A "${USER_AGENT}" \
     -X POST \
     -fsS ${HOSTER_URL}/1.0/environment/control/rest/redeploycontainersbygroup \
-    -d "appid=${APPID}&session=${session}&envName=${envName}&tag=latest&nodeGroup=${deployGroup}&useExistingVolumes=true&delay=20")
+    -d "appid=${APPID}&session=${session}&envName=${envName}&tag=${tag}&nodeGroup=${deployGroup}&useExistingVolumes=true&delay=20")
   # TODO: when the redeploy has worked, the exitOnFail interprets that as an error
   # exitOnFail $cmd
   echo "Environment redeployed" >&2
-}
-
-hasVCSProject() {
-  local session=$1
-  local envName=$2
-  local deployGroup=$3
-  local context=$4
-  echo "Getting VCS project context <$context> from group <$deployGroup> of environment <$envName>" >&2
-  local cmd=$(curl -k \
-    -H "${CONTENT_TYPE}" \
-    -A "${USER_AGENT}" \
-    -X POST \
-    -fsS ${HOSTER_URL}/1.0/environment/vcs/rest/getproject \
-    -d "envName=${envName}&session=${session}&nodeGroup=${deployGroup}&context=${context}")
-  local result=$(getCommandResult $cmd)
-  [[ "$result" == "0" ]] && echo 1 || echo 0
-  echo "Got VCS project" >&2
-  
-}
-
-updateVCSProject() {
-  local session=$1
-  local envName=$2
-  local deployGroup=$3
-  local context=$4
-  echo "Updating VCS project context <$context> from group <$deployGroup> of environment <$envName>" >&2
-  local cmd=$(curl -k \
-    -H "${CONTENT_TYPE}" \
-    -A "${USER_AGENT}" \
-    -X POST \
-    -fsS ${HOSTER_URL}/1.0/environment/vcs/rest/update \
-    -d "envName=${envName}&session=${session}&nodeGroup=${deployGroup}&context=${context}")
-  exitOnFail $cmd
-  echo "Updated VCS project" >&2
 }
 
 deployToJelastic() {
   ENVS=$(getEnvs $SESSION)
   CREATED=$(wasEnvCreated "$ENVS" "${ENV_NAME}")
 
-  # TODO: if the manifest changed (according to git history), then re-install the environment, don't just redeploy it
-
   if [ "${CREATED}" == "null" ]; then
     installEnv $SESSION "${ENV_NAME}" "$MANIFEST"
   else
     startEnvIfNecessary $SESSION "${ENV_NAME}" "$ENVS"
-    hasVCS=$(hasVCSProject $SESSION "${ENV_NAME}" ${DEPLOY_GROUP} $CONTEXT)
-    if [ "$hasVCS" == "0" ] ; then
-      redeployEnvironment $SESSION "${ENV_NAME}" ${DEPLOY_GROUP}
-    else
-      updateVCSProject $SESSION "${ENV_NAME}" ${DEPLOY_GROUP} $CONTEXT
-    fi
+    redeployEnvironment $SESSION "${ENV_NAME}" ${DEPLOY_GROUP} ${TAG}
   fi
 
   exit 0
