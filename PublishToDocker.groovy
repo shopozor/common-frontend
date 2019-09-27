074562175cdae897084ef47b6d62b638bbb31163
Laurent Michel <laurent.michel@ruag.com>
2019-09-27 07:27:23 +0200

    add IMAGE_TYPE information to docker image tag

--- PublishToDocker.groovy
+++ PublishToDocker.groovy
@@ -2,14 +2,14 @@ pipeline {
   agent any
   environment {
     DOCKER_CREDENTIALS = credentials('docker-credentials')
-    DOCKER_REPO = "softozor/shopozor-$REPO:$BRANCH"
+    DOCKER_REPO = "softozor/shopozor-$REPO:$IMAGE_TYPE-$BRANCH"
   }
   stages {
     stage('Build docker image') {
       steps {
         script {
-            sh "cp production/Dockerfile ."
-            sh "cp production/.dockerignore ."
+            sh "cp $IMAGE_TYPE/Dockerfile ."
+            sh "cp $IMAGE_TYPE/.dockerignore ."
             sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
             sh "docker build --build-arg GRAPHQL_API=$GRAPHQL_API --build-arg ENABLE_DEV_TOOLS=$ENABLE_DEV_TOOLS --network=host -t $DOCKER_REPO ."
         }
