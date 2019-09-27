7cba6a30b909dfcab8f36087b8e82269cc4b05e4
Laurent Michel <laurent.michel@ruag.com>
2019-09-27 06:08:35 +0200

    move docker publication code to common

---
+++ PublishToDocker.groovy
@@ -0,0 +1,26 @@
+pipeline {
+  agent any
+  environment {
+    DOCKER_CREDENTIALS = credentials('docker-credentials')
+    DOCKER_REPO = "softozor/shopozor-$REPO:$BRANCH"
+  }
+  stages {
+    stage('Build docker image') {
+      steps {
+        script {
+            sh "cp production/Dockerfile ."
+            sh "cp production/.dockerignore ."
+            sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
+            sh "docker build --build-arg GRAPHQL_API=$GRAPHQL_API --build-arg ENABLE_DEV_TOOLS=$ENABLE_DEV_TOOLS --network=host -t $DOCKER_REPO ."
+        }
+      }
+    }
+    stage('Publish docker image') {
+      steps {
+        script {
+            sh "docker push $DOCKER_REPO"
+        }
+      }
+    }
+  }
+}
