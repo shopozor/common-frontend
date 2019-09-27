4d8da8de4b89764040e2e61dceb8eb2a80765316
Laurent Michel <laurent.michel@ruag.com>
2019-09-27 08:20:43 +0200

    use jenkins shared lib

--- PublishToDocker.groovy
+++ PublishToDocker.groovy
@@ -1,24 +1,15 @@
+def helpers = new ch.softozor.pipeline.Helpers()
+
 pipeline {
   agent any
   environment {
     DOCKER_CREDENTIALS = credentials('docker-credentials')
-    DOCKER_REPO = "softozor/shopozor-$REPO:$IMAGE_TYPE-$BRANCH"
   }
   stages {
-    stage('Build docker image') {
+    stage('Build and publish docker image') {
       steps {
         script {
-            sh "cp $IMAGE_TYPE/Dockerfile ."
-            sh "cp $IMAGE_TYPE/.dockerignore ."
-            sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
-            sh "docker build --build-arg GRAPHQL_API=$GRAPHQL_API --build-arg ENABLE_DEV_TOOLS=$ENABLE_DEV_TOOLS --network=host -t $DOCKER_REPO ."
-        }
-      }
-    }
-    stage('Publish docker image') {
-      steps {
-        script {
-            sh "docker push $DOCKER_REPO"
+          helpers.publishDockerImage(REPO, BRANCH, GRAPHQL_API, ENABLE_DEV_TOOLS, IMAGE_TYPE)
         }
       }
     }
