6a6ae3fd418df3bdf4cc638120b38a1e469cf420
Laurent Michel <laurent.michel@ruag.com>
2019-09-30 12:31:05 +0200

    use generateSpecification method from jenkins shared lib

--- cypress/Spec.groovy
+++ cypress/Spec.groovy
@@ -6,9 +6,7 @@ pipeline {
         SOFTOZOR_CREDENTIALS = credentials('softozor-credentials')
       }
       steps {
-        sh "mono /opt/pickles/Pickles.exe --feature-directory=$WORKSPACE/cypress/e2e --output-directory=specification --system-under-test-name=shopozor-$REPO --system-under-test-version=0.0.0 --language=fr --documentation-format=dhtml --exp --et 'in-preparation'"
-        sh "sshpass -p $SOFTOZOR_CREDENTIALS_PSW ssh -o StrictHostKeyChecking=no $SOFTOZOR_CREDENTIALS_USR@softozor.ch 'rm -Rf ~/www/www.softozor.ch/shopozor/$REPO/*'"
-        sh "sshpass -p $SOFTOZOR_CREDENTIALS_PSW scp -o StrictHostKeyChecking=no -r specification/* $SOFTOZOR_CREDENTIALS_USR@softozor.ch:~/www/www.softozor.ch/shopozor/$REPO/"
+        helpers.generateSpecification("$WORKSPACE/cypress/e2e", $REPO, GIT_COMMIT)
       }
     }
   }
